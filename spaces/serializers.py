1f5a2ebee2cfa444eecc3cd68bcd227b30fa8f0b
Tulipurple <164765964+Tulipurple@users.noreply.github.com>
2025-07-11 06:46:43 +0900

    feat: space 수정/삭제

--- spaces/serializers.py
+++ spaces/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Space
 
 
+# 루트 안의 공간 생성
 class SpaceCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Space
@@ -18,6 +19,23 @@ class SpaceCreateSerializer(serializers.ModelSerializer):
         ]
 
 
+# 루트 공간 수정
+class SpaceUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Space
+        fields = [
+            "space_name",
+            "space_type",
+            "start_x",
+            "start_y",
+            "width",
+            "height",
+            "size",
+            "direction",
+        ]
+
+
+# 공간 정보 응답
 class SpaceResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Space
--- spaces/views.py
+++ spaces/views.py
@@ -1,15 +1,20 @@
-from turtle import st
 from rest_framework import status, serializers
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from django.http import Http404
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, RetrieveUpdateDestroyAPIView
 from django.shortcuts import get_object_or_404
-from .serializers import SpaceCreateSerializer, SpaceResponseSerializer
+from .serializers import (
+    SpaceCreateSerializer,
+    SpaceResponseSerializer,
+    SpaceUpdateSerializer,
+)
 from .models import Space
 from groups.models import Group
 
 
-# Create your views here.
+# 루트에서 공간 생성
 class SpaceCreateView(GenericAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = SpaceCreateSerializer
@@ -34,7 +39,7 @@ class SpaceCreateView(GenericAPIView):
 
         spaces = [Space(group=group, **data) for data in serializer.validated_data]
         created = Space.objects.bulk_create(spaces)
-
+        # 여러 공간 한꺼번에 DB 저장
         response_data = SpaceResponseSerializer(created, many=True).data
 
         return Response(
@@ -44,4 +49,55 @@ class SpaceCreateView(GenericAPIView):
                 "data": {"root": response_data},
             },
             status=status.HTTP_201_CREATED,
+        )
+
+
+# 루트에서 공간 수정/삭제
+class SpaceRUDAPIView(RetrieveUpdateDestroyAPIView):
+    # Update, Delete만 있음
+
+    permission_classes = [IsAuthenticated]
+    # URL에서 받아올 space_id를 기준으로 함
+    lookup_field = "space_id"
+    lookup_url_kwarg = "space_id"
+    queryset = Space.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method.lower() in ("patch"):
+            return SpaceUpdateSerializer
+        return SpaceResponseSerializer
+
+    def get_object(self):
+        try:
+            return super().get_object()
+        except Http404:
+            space_id = self.kwargs.get("space_id")
+            raise NotFound(
+                {
+                    "success": True,
+                    "errorCode": "SPACE_NOT_FOUND",
+                    "message": f"해당 ID에 해당하는 공간이 존재하지 않습니다. {space_id}",
+                }
+            )
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        return Response(
+            {
+                "success": True,
+                "message": "공간이 성공적으로 수정되었습니다",
+                "data": response.data,
+            },
+            status=status.HTTP_200_OK,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        return Response(
+            {
+                "success": True,
+                "message": "공간이 성공적으로 삭제되었습니다",
+                "data": {},
+            },
+            status=status.HTTP_204_NO_CONTENT,
         )
