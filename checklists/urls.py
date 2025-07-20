86c8f1f5416fe260c9c27b870face08b3ba74c91
suuu004 <149984963+suuu004@users.noreply.github.com>
2025-07-21 00:10:51 +0900

    체크리스트 url 수정

--- checklists/urls.py
+++ checklists/urls.py
@@ -9,7 +9,7 @@ from .views import (
 
 urlpatterns = [
     path('spaces/top-checklists/', PrioritySpaceView.as_view(), name='top3-spaces'),
-    path('spaces/<int:space_id>/checklists/', ChecklistSpaceView.as_view(), name='space-checklists'),
+    path('spaces/<int:space_id>/checklist/', ChecklistSpaceView.as_view(), name='space-checklists'),
     path('checklist-items/<int:checklist_item_id>/complete/', ChecklistCompleteView.as_view(), name='checklist-item-complete'),
     path('create/', ChecklistCreateView.as_view(), name='checklist-item-create'),
     path('checklist-items/<int:checklist_item_id>/delete/', ChecklistDeleteView.as_view(), name='checklist-item-delete'),
